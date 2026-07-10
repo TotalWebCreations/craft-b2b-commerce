be040a539ead1fb6284f2147b8b6d035bc6317c5
Bryan Nijhof <bryan@totalwebcreations.nl>
2026-07-10 22:52:39 +0200

    fix: remove duplicate 'Download PDF' NL translation key

--- src/translations/nl/b2b-commerce.php
+++ src/translations/nl/b2b-commerce.php
@@ -480,6 +480,5 @@ return [
     '31–60 days' => '31–60 dagen',
     '61–90 days' => '61–90 dagen',
     '90+ days' => '90+ dagen',
-    'Download PDF' => 'PDF downloaden',
     'As of' => 'Per',
 ];
